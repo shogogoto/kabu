f16cecfd212a29c6a68f0f9122f37e5efddf2813
Shogo Goto <gotoadmn0605@gmail.com>
2025-09-04 16:17:14 +0900

    実株価と理論株価が同じなときの画像出力

--- kabu/shared/visualize.py
+++ kabu/shared/visualize.py
@@ -1,7 +1,10 @@
 """可視化."""
 
+from collections.abc import Callable
+
 import pandas as pd
 from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
 
 
 def save_image():
@@ -23,11 +26,29 @@ def save_image():
     # plt.grid(True)
 
 
-def saveimg(df: pd.DataFrame, title: str):
-    plt.figure(figsize=(10, 6))
-    plt.plot(df.index, df)
-    plt.title(title)
-    plt.xlabel("Date")
-    plt.ylabel("EPS")
-    plt.grid(True)
-    plt.savefig(f"{title}.png")
+def saveimg(
+    df: pd.DataFrame,
+    title: str,
+    xlabel: str = "Date",
+    ylabel: str = "Price",
+    proc_ax: Callable[[Axes], None] = lambda _ax: None,
+):
+    """Save plot as image."""
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(df.index, df)
+    # plt.title(title)
+    # plt.xlabel("Date")
+    # plt.ylabel("EPS")
+    # plt.grid(True)
+    # plt.savefig(f"{title}.png")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    df.plot(ax=ax, title=title, grid=True)
+
+    proc_ax(ax)
+
+    ax.grid(True)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    fig.savefig(f"{title}.png")
+    plt.close(fig)
